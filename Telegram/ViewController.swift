7c7526da222d8e70f7016d9c59e7a3262af4e7bf
ph0nky <earthphonky@gmail.ru>
2025-11-17 12:59:51 +0300

    Initial Commit

---
+++ Telegram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegram
+//
+//  Created by Кирилл on 17.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
